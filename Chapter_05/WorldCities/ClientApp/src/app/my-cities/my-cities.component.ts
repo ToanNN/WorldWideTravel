e67d4232ddccf9227b2f6ea6622897fc79408743
Toan Nguyen <toan.1aus@gmail.com>
2021-12-14 13:38:16 +1000

    Debounce filtered

--- Chapter_05/WorldCities/ClientApp/src/app/my-cities/my-cities.component.ts
+++ Chapter_05/WorldCities/ClientApp/src/app/my-cities/my-cities.component.ts
@@ -2,6 +2,8 @@ import { HttpClient, HttpParams } from '@angular/common/http';
 import { Component, Inject, OnInit, ViewChild } from '@angular/core';
 import { MatPaginator, PageEvent } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
+import { Subject } from 'rxjs';
+import { debounceTime, distinctUntilChanged } from 'rxjs/operators';
 import { MyCity } from './MyCity';
 
 @Component({
@@ -10,31 +12,49 @@ import { MyCity } from './MyCity';
   styleUrls: ['./my-cities.component.css']
 })
 export class MyCitiesComponent implements OnInit {
-  public displayedColumns: string[] =['id', 'name','lat', 'lon'];
+  public displayedColumns: string[] = ['id', 'name', 'lat', 'lon'];
   public cities: MatTableDataSource<MyCity>;
-  @ViewChild(MatPaginator, {static: false}) paginator: MatPaginator;
-  
-  constructor(private http: HttpClient, @Inject('BASE_URL') private baseUrl: string) { }
+  filteredText: Subject<string>;
+  @ViewChild(MatPaginator, { static: false }) paginator: MatPaginator;
+
+  constructor(private http: HttpClient, @Inject('BASE_URL') private baseUrl: string) {
+    this.filteredText = new Subject<string>();
+  }
 
   ngOnInit(): void {
     let pageEvent = new PageEvent();
     pageEvent.pageIndex = 0;
-    pageEvent.pageSize =10;
-    this.getData(pageEvent); 
+    pageEvent.pageSize = 10;
+    this.getData(pageEvent);
   }
- 
-  getData(event:PageEvent){
+
+  getData(event: PageEvent, queryText: string = null) {
     const url = this.baseUrl + 'api/cities';
     var params = new HttpParams()
-    .set("pageIndex", event.pageIndex.toString())
-    .set("pageSize", event.pageSize.toString());
+      .set("pageIndex", event.pageIndex.toString())
+      .set("pageSize", event.pageSize.toString())
+      .set("filterQuery", queryText);
 
-    this.http.get<any>(url, {params: params})
-    .subscribe(result=> {
-      this.paginator.length = result.totalCount;
-      this.paginator.pageIndex = result.pageIndex;
-      this.paginator.pageSize = result.pageSize;
-      this.cities = new MatTableDataSource<MyCity>(result.data);
-    });
+    this.http.get<any>(url, { params: params })
+      .subscribe(result => {
+        this.paginator.length = result.totalCount;
+        this.paginator.pageIndex = result.pageIndex;
+        this.paginator.pageSize = result.pageSize;
+        this.cities = new MatTableDataSource<MyCity>(result.data);
+      });
   }
+
+  onFilterTextChanged(filterText: string) {
+    if (this.filteredText.observers.length == 0) {
+      this.filteredText.pipe(debounceTime(5000), distinctUntilChanged())
+        .subscribe(query => {
+          let pageEvent = new PageEvent();
+          pageEvent.pageIndex = 0;
+          pageEvent.pageSize = 25;
+          this.getData(pageEvent, query);
+        })
+    }
+  }
+
+
 }
